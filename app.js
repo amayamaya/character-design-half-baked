95db9476da882795349f9458594d5252d8b299fc
‘AMAYA <aamaya547@gmail.com>
2022-04-28 15:20:16 -0700

    CI test

--- app.js
+++ app.js
@@ -6,70 +6,41 @@ const headEl = document.getElementById('head');
 const middleEl = document.getElementById('middle');
 const bottomEl = document.getElementById('bottom');
 const reportEl = document.getElementById('report');
-const catchphrasesEl = document.getElementById('catchphrases');
-// const catchphraseInput = document.getElementById('catchphrase-input');
+const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
+const catchphrasesEl = document.getElementById('catchphrases');
 // const catchphrase = document.getElementById('catchphrase');
 // const catchphraseList = document.getElementById('catchphrase-list');
-// set state for how many times the user changes the head, middle, and bottom
 
 let headReport = 0;
 let middleReport = 0;
 let bottomReport = 0;
-// set state for all of the character's catchphrases
 let catchphrases = [];
 
 headDropdown.addEventListener('change', () => {
-    // get the value of the head dropdown
     // console.log('changing select', headDropdown.value);
-    // increment the head change count state
     headReport++;
     // console.log(report);
-    // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
     headEl.style.backgroundImage = `url('./assets/${headDropdown.value}-head.png')`;
-    // update the stats to show the new count (call displayStats() to do this work)
     displayStats();
 });
-
 
 middleDropdown.addEventListener('change', () => {
-    // get the value of the middle dropdown
-    // console.log('changing select', middleDropdown.value);
-    // increment the middle change count state
     middleReport++;
-    // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
     middleEl.style.backgroundImage = `url('./assets/${middleDropdown.value}-middle.png')`;
-    // update the stats to show the new count (call displayStats() to do this work)
     displayStats();
 });
-
-// function.displayStats() {
-//     reportEl.textContent = `You have changed the middle ${report} times.`
-// };
 
 bottomDropdown.addEventListener('change', () => {
-    // get the value of the bottom dropdown
-    // console.log('changing select', bottomDropdown.value);
-    // increment the bottom change count state
     bottomReport++;
-    // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
     bottomEl.style.backgroundImage = `url('./assets/${bottomDropdown.value}-pants.png')`;
-    // update the stats to show the new count (call displayStats() to do this work)
     displayStats();
 });
 
-// function.displayStats() {
-//     reportEl.textContent = `You have changed the bottom ${report} times.`
-// };
-
 catchphraseButton.addEventListener('click', () => {
-    // get the value of the catchphrase input
     catchphrases.push(catchphraseInput.value);
     // console.log('changing input', catchphraseInput.value);
     // console.log(catchphrases);
-    // push the new catchphrase to the catchphrase array in state
-    // clear out the form input's value so it's empty to the user
-    // update the dom to show the new catchphrases (refactor to/call displayCatchphrases to do this work)
     displayCatchphrases();
 });
 
@@ -78,9 +49,7 @@ function displayStats() {
 }
 
 function displayCatchphrases() {
-    // clear out the DOM for the currently displayed catchphrases
     catchphrasesEl.textContent = '';
-    // loop through each catchphrase in state
     for (let catchphrase of catchphrases) {
         const li = document.createElement('div');
         li.textContent = catchphrase;
@@ -88,9 +57,4 @@ function displayCatchphrases() {
     }
 }
    
-    // and for each catchphrase
-    
-    // create an HTML element with the catchphrase as its text content
-    
-    // and append that HTML element to the cleared-out DOM
 
